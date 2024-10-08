a8da253df3a255c44e0201dabae659188804c05b
Safi <safiullahkakar2020@outlook.com>
2024-10-08 15:53:32 +0430

    Added stage 1 solution code

--- app/src/main/kotlin/tasklist/SimpleTasks.kt
+++ app/src/main/kotlin/tasklist/SimpleTasks.kt
@@ -1,2 +1,29 @@
 package tasklist
 
+import java.util.Scanner
+
+fun printTasks(taskList: List<String>) {
+    if (taskList.isEmpty()) {
+        println("No tasks have been input")
+    } else {
+        taskList.forEachIndexed { index, item ->
+            val taskNumber = index + 1
+            println(taskNumber.toString() + (if (taskNumber < 10) "  " else " ") + item)
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+
+    println("Input the tasks (enter a blank line to end):")
+    val tasks = mutableListOf<String>()
+
+    var line = sc.nextLine()
+    while (line.isNotBlank()) {
+        tasks.add(line.trim())
+        line = sc.nextLine()
+    }
+    printTasks(tasks)
+    sc.close()
+}
